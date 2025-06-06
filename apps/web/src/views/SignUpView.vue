aba7efdf4b222c2cade39c0c226938cb878010ce
IAmKou <luongcu111@gmail.com>
2025-06-06 21:46:13 +0700

    conflict test

--- apps/web/src/views/SignUpView.vue
+++ apps/web/src/views/SignUpView.vue
@@ -22,6 +22,10 @@
       <button type="submit" class="submit-button">Sign up</button>
     </form>
 
+    <div>
+      <h2>conflict maybe</h2>
+    </div>
+
     <footer class="footer">© 2025 HereToTranslate</footer>
   </div>
 </template>

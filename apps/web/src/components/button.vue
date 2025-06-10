e2c93d9354c1276785cfa322d96913083a624e8e
toobi-svg <richchoi6@gmail.com>
2025-06-10 23:08:42 +0700

    uơdate primevue

---
+++ apps/web/src/components/button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Button label="Save" icon="pi pi-save" />
+    <InputText v-model="name" placeholder="Enter your name" />
+  </div>
+</template>
+
+<script setup>
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import { ref } from 'vue'
+
+const name = ref('')
+</script>

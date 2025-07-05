b435f47e9ec0e8407c7e33ddcebcd6401233f5dc
IAmKou <luongcu111@gmail.com>
2025-07-05 15:35:39 +0700

    forgot import schema

--- apps/server/src/db/mongo/mongo.module.ts
+++ apps/server/src/db/mongo/mongo.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatMessageSchema } from './schema/chat-message.schema';
 import { ChatRoomSchema } from './schema/chat-room.schema';
+import { TranslationStringSchema } from './schema/translation.schema';
+
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'ChatMessage', schema: ChatMessageSchema },
-      { name: 'ChatRoom', schema: ChatRoomSchema }
+      { name: 'ChatRoom', schema: ChatRoomSchema },
+      { name: 'TranslationString', schema: TranslationStringSchema },
     ])
   ],
   exports: [MongooseModule]

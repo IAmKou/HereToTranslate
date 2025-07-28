1db163f651dc9d9e7bbce03cbd916184403a1c0c
IAmKou <luongcu111@gmail.com>
2025-07-28 22:12:36 +0700

    Update managers.module.ts

--- apps/server/src/manager/managers.module.ts
+++ apps/server/src/manager/managers.module.ts
@@ -52,7 +52,8 @@ import { ManifestService } from '#LocalProject/Managers/service/manifest.service
 import { NotificationManagerService } from '#LocalProject/Managers/service/notification-manager.service';
 import { NotificationController } from '#LocalProject/Managers/controller/notification.controller';
 import { BullModule } from '@nestjs/bull';
-
+import { AiChatService } from '#LocalProject/Managers/service/ai-manager.service';
+import { AiChatController } from '#LocalProject/Managers/controller/ai-chat.controller';
 
 @Global()
 @Module({
@@ -103,6 +104,7 @@ import { BullModule } from '@nestjs/bull';
     TaskGateway,
     ManifestService,
     NotificationManagerService,
+    AiChatService,
   ],
   exports: [
     CategoryManagerService,
@@ -123,6 +125,7 @@ import { BullModule } from '@nestjs/bull';
     TaskManagerService,
     ManifestService,
     NotificationManagerService,
+    AiChatService,
   ],
   controllers: [
     CategoryController,
@@ -140,7 +143,8 @@ import { BullModule } from '@nestjs/bull';
     TranslationController,
     AdminTransactionController,
     ChatController,
-    NotificationController
+    NotificationController,
+    AiChatController,
   ]
 })
 export class ManagersModule {
